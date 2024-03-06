c289105ff64c041c6e266ce1d100b3f437221b67
Joris Gillis <joris@yacoda.com>
2024-03-06 15:32:36 +0100

    visual finetuning

--- driver.sh
+++ driver.sh
@@ -1,17 +1,27 @@
 #!/bin/bash
 
+
+
 pip install casadi
 # Available on Win/Linux/Mac
 # Includes Ipopt
+
+
+
 
 # Generate C code
 # Can also be done from Matlab
 python generate.py
 
+
+
 # Compile C code
 export  P=`python -c "import casadi as ca;print(ca.GlobalOptions.getCasadiPath())"`
 export IP=`python -c "import casadi as ca;print(ca.GlobalOptions.getCasadiIncludePath())"`
 gcc main.c F.c -I${IP} -lipopt -L${P} -Wl,-rpath ${P} -lm -o main
+
+
+
 
 # Run
 ./main
--- generate.py
+++ generate.py
@@ -1,7 +1,7 @@
 import casadi as ca
 import numpy as np
 
-opti = ca.Opti()
+opti   = ca.Opti()
 
 center = opti.variable(2)
 radius = opti.variable()
@@ -11,7 +11,7 @@ a      = opti.parameter()
 opti.minimize(-radius)
 
 # Sample edge vertices
-ts = np.linspace(0,2*np.pi,1000)
+ts = np.linspace(0, 2*np.pi, 1000)
 v_x = radius*np.cos(ts)+center[0]
 v_y = radius*np.sin(ts)+center[1]
 
@@ -19,14 +19,18 @@ opti.subject_to(v_x>=0)
 opti.subject_to(v_y>=a*np.sqrt(v_x))
 opti.subject_to(v_x**2+v_y**2<=1)
 
-opti.set_initial(center,[0.5,0.5])
+opti.set_initial(center, [0.5, 0.5])
+opti.set_value(a, 1)
 
 opti.solver('ipopt')
-F = opti.to_function('F',[a],[np.pi*radius**2,center])
 
-print(F(1)) # 0.143661
+# Normal use
+sol = opti.solve()
+print(sol.value(radius))
 
+# Codegen via a CasADi Function
+F = opti.to_function('F',\
+        [a],\
+        [np.pi*radius**2, center])
 F.generate('F.c',{"with_header":True})
-
-
 
